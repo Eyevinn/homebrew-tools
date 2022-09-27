009c43bcad9e92e1c4ad27b0f7c7d14a678655e6
Jonas Birmé <jonas.birme@eyevinn.se>
2022-09-27 11:16:20 +0000

    whpp-play: add 0.1.0 bottle.

--- Formula/whpp-play.rb
+++ Formula/whpp-play.rb
@@ -5,6 +5,13 @@ class WhppPlay < Formula
   sha256 "6d321a1c8e8ef10fd650bee5c36d2d863545d4841bdd43f4576f120749954d2a"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/Eyevinn/homebrew-tools/releases/download/whpp-play-0.1.0"
+    rebuild 1
+    sha256 cellar: :any,                 big_sur:      "71ca3eea7199ed678850c74bc124d37647af35cd72bae144b5d5de11b3e1d60c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b7bb159f40a093decf8469f455c5b4c2e293d0e688167ebc86cef36e2eed2cf7"
+  end
+
   depends_on "cmake" => :build
   depends_on "gst-plugins-bad"
   depends_on "gst-plugins-base"
