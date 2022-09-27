654a78168e46f0ebe87bd20b96042432caabebe6
Jonas Birmé <jonas.birme@eyevinn.se>
2022-09-27 12:05:36 +0200

    whpp-play: removed useless assignment

--- Formula/whpp-play.rb
+++ Formula/whpp-play.rb
@@ -17,7 +17,7 @@ class WhppPlay < Formula
   end
 
   test do
-    output = shell_output("#{bin}/whpp-play", result = 1).strip
+    output = shell_output("#{bin}/whpp-play", 1).strip
     assert_match "Usage: ", output
   end
 end
