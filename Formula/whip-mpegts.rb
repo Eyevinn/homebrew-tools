2b68e034ef382b4735c7a9edf7f768db5395b7ce
Jonas Birmé <jonas.birme@eyevinn.se>
2022-11-02 15:40:05 +0100

    whip-mpegts 0.3.0 (updated)

--- Formula/whip-mpegts.rb
+++ Formula/whip-mpegts.rb
@@ -1,15 +1,9 @@
 class WhipMpegts < Formula
   desc "MPEG-TS WHIP client"
   homepage "https://github.com/Eyevinn/whip-mpegts"
-  url "https://github.com/Eyevinn/whip-mpegts/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "dee7027737b4bbfcf466fb49b56a500967d7fb8c94d36c4caaceabd8542864b5"
+  url "https://github.com/Eyevinn/whip-mpegts/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "e8c7f315ca13b1d47832a320cb07a26d351a65e4aa3ade1abfe4bd7ab6c14c0e"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/Eyevinn/homebrew-tools/releases/download/whip-mpegts-0.1.3"
-    sha256 cellar: :any,                 big_sur:      "825ec6f81bd0b0cd82403efec66c5fd10019338691c66d1d76b3059d1c0c30d1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5dd161e9e5cec283a1e63e80a70946f9ad21e8951e64442c51bab453f3cdac58"
-  end
 
   depends_on "cmake" => :build
   depends_on "gst-libav"
