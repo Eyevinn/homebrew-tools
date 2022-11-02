d4d19cb0c65dfa5dfe9c5c7032e43229c88ac93d
Jonas Birmé <jonas.birme@eyevinn.se>
2022-11-02 16:12:29 +0100

    whip-mpegts 0.4.0
    whip-mpegts 0.4.0 (release)
    
    Closes #5.
    
    Signed-off-by: Jonas Birmé <jonas.birme@eyevinn.se>

--- Formula/whip-mpegts.rb
+++ Formula/whip-mpegts.rb
@@ -1,14 +1,9 @@
 class WhipMpegts < Formula
   desc "MPEG-TS WHIP client"
   homepage "https://github.com/Eyevinn/whip-mpegts"
-  url "https://github.com/Eyevinn/whip-mpegts/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "e8c7f315ca13b1d47832a320cb07a26d351a65e4aa3ade1abfe4bd7ab6c14c0e"
+  url "https://github.com/Eyevinn/whip-mpegts/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "7cce8b3c57627e12f2138ff7d09b8f7aa7002d048f426cd65a6cbf4646a483ed"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/Eyevinn/homebrew-tools/releases/download/whip-mpegts-0.3.0"
-    sha256 cellar: :any, big_sur: "0b6e990cfd53a702278ef98708cf899439fc2b783c8c6a7949c1b25b8d4b735a"
-  end
 
   depends_on "cmake" => :build
   depends_on "gst-libav"
