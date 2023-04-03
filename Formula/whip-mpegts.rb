515c8e0c8dfee21ff52fd4c02b70fd313b8572a1
Jonas Birmé <jonas.birme@eyevinn.se>
2023-04-03 10:40:40 +0200

    whip-mpegts 0.7.1
    * whip-mpegts 0.7.0: new version
    * whip-mpegts 0.7.1: fix Linux build
    
    Closes #7.
    
    Signed-off-by: Jonas Birmé <jonas.birme@eyevinn.se>

--- Formula/whip-mpegts.rb
+++ Formula/whip-mpegts.rb
@@ -1,15 +1,9 @@
 class WhipMpegts < Formula
   desc "MPEG-TS WHIP client"
   homepage "https://github.com/Eyevinn/whip-mpegts"
-  url "https://github.com/Eyevinn/whip-mpegts/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "e8e59e74cd294ab819e2b08ffee66bd9a3600d672e8ac519b075ccad30ff8996"
+  url "https://github.com/Eyevinn/whip-mpegts/archive/refs/tags/v0.7.1.tar.gz"
+  sha256 "bdeb5be8495a55e9d9a129851dee30bcb1c10538ef29fc09a311faacedb86aed"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/Eyevinn/homebrew-tools/releases/download/whip-mpegts-0.5.0"
-    sha256 cellar: :any,                 big_sur:      "c63a6e82d595acd869bdb631f82664d85687b17508616ef0565642d98cbb6152"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "f35d8ff85d84ae5b38eaf0b450c5c28b29440a86b33326162a37684372abe827"
-  end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
