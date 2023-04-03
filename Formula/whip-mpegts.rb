75007c0e77172f56b4e9e770860d35c5925a9d4d
Jonas Birmé <jonas.birme@eyevinn.se>
2023-04-03 18:32:24 +0200

    whip-mpegts: add 0.7.1 bottle.

--- Formula/whip-mpegts.rb
+++ Formula/whip-mpegts.rb
@@ -5,6 +5,12 @@ class WhipMpegts < Formula
   sha256 "bdeb5be8495a55e9d9a129851dee30bcb1c10538ef29fc09a311faacedb86aed"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/Eyevinn/homebrew-tools/releases/download/whip-mpegts-0.7.1"
+    sha256 cellar: :any,                 monterey:     "65764d3406f7dff2f786d675b7adaf63948a561537bbef9b5c74fcab97fc40ec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4197c05f1922cf503d144f3f171514bf95fa9f2c28e730401980ad9d8da56ab2"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "gst-libav"
