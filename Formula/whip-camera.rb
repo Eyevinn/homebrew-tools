69c4b1c168630e5a8798f25ea0c2ec799c12a1c5
Jonas Birmé <jonas.birme@eyevinn.se>
2023-04-04 13:57:19 +0200

    chore: fix style issues
    Closes #9.
    
    Signed-off-by: Jonas Birmé <jonas.birme@eyevinn.se>

--- Formula/whip-camera.rb
+++ Formula/whip-camera.rb
@@ -2,16 +2,16 @@ class WhipCamera < Formula
   desc "Camera WHIP client"
   homepage "https://github.com/Eyevinn/whip-camera"
   url "https://github.com/Eyevinn/whip-camera/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "3ff2de84126ee3ada3f03700e60e5285df291dc86a367d1848b046a76157077f "
+  sha256 "3ff2de84126ee3ada3f03700e60e5285df291dc86a367d1848b046a76157077f"
   license "Apache-2.0"
 
   depends_on "cmake" => :build
-  depends_on "pkg-config" => :build
   depends_on "glib" => :build
+  depends_on "pkg-config" => :build
   depends_on "gst-libav"
   depends_on "gst-plugins-bad"
-  depends_on "gst-plugins-good"
   depends_on "gst-plugins-base"
+  depends_on "gst-plugins-good"
   depends_on "gstreamer"
   depends_on "libsoup@2"
 
