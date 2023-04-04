8934adfaed3f24abf837d47e809af2b9ac9911e6
Jonas Birmé <jonas.birme@eyevinn.se>
2023-04-04 14:46:13 +0200

    whip-camera: add 0.1.0 bottle.

--- Formula/whip-camera.rb
+++ Formula/whip-camera.rb
@@ -5,6 +5,12 @@ class WhipCamera < Formula
   sha256 "3ff2de84126ee3ada3f03700e60e5285df291dc86a367d1848b046a76157077f"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/Eyevinn/homebrew-tools/releases/download/whip-camera-0.1.0"
+    sha256 cellar: :any,                 monterey:     "a8d49c73cc8cfe59feaf1a53afc3d1471f19d18c938cc463c8a657f522be5ed9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "688821b61ebf8956f2f92849cb9c3cc26ea1be04988560cc298de62893c463b4"
+  end
+
   depends_on "cmake" => :build
   depends_on "glib" => :build
   depends_on "pkg-config" => :build
