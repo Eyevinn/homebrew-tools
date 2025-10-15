8cdbd3d5e5af84bb73d753059a78a1c93228a7e2
Björn Westman <bjorn.westman@svt.se>
2025-10-15 11:26:04 +0200

    Add libsoup@2 dependency for whip-mpegts 0.9.1

--- Formula/whip-mpegts.rb
+++ Formula/whip-mpegts.rb
@@ -8,6 +8,7 @@ class WhipMpegts < Formula
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "gstreamer"
+  depends_on "libsoup@2"
 
   def install
     system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-G", "Unix Makefiles", ".", *std_cmake_args
