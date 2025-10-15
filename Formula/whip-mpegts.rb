1016eeff80d6cf74b3e5a06c17e6d6158ce906e5
Sandra Larsson <Sandra.larsson@eyevinn.se>
2025-10-15 13:12:28 +0200

    fix: update to whip-mpegts version 0.9.1 (#10)

--- Formula/whip-mpegts.rb
+++ Formula/whip-mpegts.rb
@@ -1,21 +1,23 @@
 class WhipMpegts < Formula
   desc "MPEG-TS WHIP client"
   homepage "https://github.com/Eyevinn/whip-mpegts"
-  url "https://github.com/Eyevinn/whip-mpegts/archive/refs/tags/v0.7.1.tar.gz"
-  sha256 "bdeb5be8495a55e9d9a129851dee30bcb1c10538ef29fc09a311faacedb86aed"
+  url "https://github.com/Eyevinn/whip-mpegts/archive/refs/tags/v0.9.1.tar.gz"
+  sha256 "25d99e66e07f4ce984353ab6afd227e4bb28a9982fd63842ab5af169a4cbf220"
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/Eyevinn/homebrew-tools/releases/download/whip-mpegts-0.7.1"
-    sha256 cellar: :any,                 monterey:     "65764d3406f7dff2f786d675b7adaf63948a561537bbef9b5c74fcab97fc40ec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "4197c05f1922cf503d144f3f171514bf95fa9f2c28e730401980ad9d8da56ab2"
+    root_url "https://github.com/Eyevinn/homebrew-tools/releases/download/whip-mpegts-0.9.1"
+    rebuild 1
+    sha256 cellar: :any, arm64_sequoia: "9f65c9f6ee466d6975c3344fdb8e3be7f1efe0934ae51a6249facb29d9bddae7"
   end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "gstreamer"
+  depends_on "libsoup@2"
 
   def install
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libsoup@2"].opt_lib/"pkgconfig"
     system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-G", "Unix Makefiles", ".", *std_cmake_args
     system "make", "install"
   end
