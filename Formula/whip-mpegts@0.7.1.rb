5de33ceeca7bbccef7e1adb436ad374c8c1c7925
Björn Westman <bjorn.westman@svt.se>
2025-10-15 12:08:15 +0200

    Fix formula order: bottle before keg_only

--- Formula/whip-mpegts@0.7.1.rb
+++ Formula/whip-mpegts@0.7.1.rb
@@ -5,13 +5,13 @@ class WhipMpegtsAT071 < Formula
   sha256 "bdeb5be8495a55e9d9a129851dee30bcb1c10538ef29fc09a311faacedb86aed"
   license "Apache-2.0"
 
-  keg_only :versioned_formula
-
   bottle do
     root_url "https://github.com/Eyevinn/homebrew-tools/releases/download/whip-mpegts-0.7.1"
     sha256 cellar: :any,                 monterey:     "65764d3406f7dff2f786d675b7adaf63948a561537bbef9b5c74fcab97fc40ec"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "4197c05f1922cf503d144f3f171514bf95fa9f2c28e730401980ad9d8da56ab2"
   end
+
+  keg_only :versioned_formula
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
